f6f42f28f7b74582840dea0d53e233a185e912a0
TEST <mryoun78@naver.com>
2025-06-04 15:47:30 +0900

    Updata

--- DCU Froject/login.js
+++ DCU Froject/login.js
@@ -1,5 +1,15 @@
 const FORM = document.querySelector('.form');
 const INPUT = document.querySelector('.input');
+const CLOCK = document.querySelector('.clock');
+
+function getClock(){
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+
+    CLOCK.innerText = `${hours}:${minutes}`;
+}
 
 function login(event){
     event.preventDefault();
@@ -14,4 +24,6 @@ function login(event){
     }
 }
 
+getClock();
+setInterval(getClock, 1000);
 FORM.addEventListener("submit", login);
